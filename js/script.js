78877884390137d0b394b0e6edecbd2258bc842c
bnd.ev <p6669949699@gmail.com>
2023-12-29 15:17:50 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("submit");
+const songResult = document.getElementById("songResult");
+const fileResult = document.getElementById("fileResult");
+const urlResult = document.getElementById("urlResult");
+const dateResult = document.getElementById("dateResult");
+
+loginButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const fileInput = loginForm.querySelector('input[type="file"]');
+  const song = loginForm.song.value;
+  const url = loginForm.url.value;
+  const date = loginForm.date.value;
+
+  const file = fileInput.files[0];
+  const imgUrl = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = imgUrl;
+  console.log(image);
+
+  if (file) {
+    fileResult.innerHTML = `<img src='${imgUrl}'>`; // Display the file name or other file details
+  } else {
+    console.error("No file selected");
+  }
+  songResult.innerText = song;
+  urlResult.innerText = url;
+  dateResult.innerText = date;
+});
