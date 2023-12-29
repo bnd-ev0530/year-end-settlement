a0d2ff9f81809a2e6a8a08012def54584254e973
bnd.ev <p6669949699@gmail.com>
2023-12-29 21:12:12 +0900

    modify ui, video function

--- js/script.js
+++ js/script.js
@@ -9,21 +9,23 @@ loginButton.addEventListener("click", (event) => {
   event.preventDefault();
   const fileInput = loginForm.querySelector('input[type="file"]');
   const song = loginForm.song.value;
-  const url = loginForm.url.value;
+  let url = loginForm.url.value;
   const date = loginForm.date.value;
 
   const file = fileInput.files[0];
   const imgUrl = URL.createObjectURL(file);
   const image = new Image();
   image.src = imgUrl;
-  console.log(image);
+
+  url = url.substr(url.lastIndexOf("=") + 1);
+  console.log(url);
 
   if (file) {
-    fileResult.innerHTML = `사진 : <img src='${imgUrl}' style="max-width:100%">`; // Display the file name or other file details
+    fileResult.innerHTML = `<h1>사진</h1><div><img src=${imgUrl} style="max-width:100%"></div>`; // Display the file name or other file details
   } else {
     console.error("No file selected");
   }
-  songResult.innerText = "노래 : " + song;
-  urlResult.innerHTML = `직캠 : <iframe width="100%" height="315" src="${url}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
-  dateResult.innerText = "날짜 : " + date;
+  songResult.innerHTML = `<h1>노래</h1><p>${song}</p>`;
+  urlResult.innerHTML = `<h1>직캠</h1><iframe width="100%" height="315" src="https://www.youtube.com/embed/${url}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+  dateResult.innerHTML = `<h1>날짜</h1><p>${date}</p>`;
 });
